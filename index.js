d29bf1b1986e4f7af240c94ffbf2a99691622860
Калдар К. Н <KaldarKN@elcom.local>
2022-09-29 19:03:59 +0700

    Залил на heroku. Сервер работает

--- index.js
+++ index.js
@@ -42,11 +42,6 @@ app.post('/comments', CommentController.createComment);
 //Получение всех комментариев
 app.get('/comments', CommentController.getAllComments);
 
-//Для проверки соединения с сервером
-app.get('/test', (req, res) => {
-  res.send('Сервер отвечает');
-});
-
 //Запускаем наш сервер на порте 4444 и в случае ошибки запуск выводим информацию в консоль
 app.listen(process.env.PORT || 4444, (err) => {
   if (err) {
